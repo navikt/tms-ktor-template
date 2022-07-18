fafdb56c58526be259b20a57556166d01bcc8599
Atle Nordland <atle.nordland@nav.no>
2022-07-18 11:35:49 +0200

    Bump versjoner

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     maven("https://jitpack.io")
 }
 
-val dittNavDependenciesVersion = "2022.01.27-13.16-8429354bdc72"
+val dittNavDependenciesVersion = "2022.04.26-13.19-0c38fa7d68db"
 
 dependencies {
     implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
