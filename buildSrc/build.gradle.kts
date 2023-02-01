ea0635ba754680d1563cb66369aa5dcd3c709677
Christopher Santana <christopher.santana@nav.no>
2023-02-01 12:45:44 +0100

    Tilpass dependencies.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,11 +4,4 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://jitpack.io")
-}
-
-val dittNavDependenciesVersion = "2022.01.27-13.16-8429354bdc72"
-
-dependencies {
-    implementation("com.github.navikt:dittnav-dependencies:$dittNavDependenciesVersion")
 }
